438b34f77a423d1fde2ff57653f92d46d49e46c3
tnyo43 <tnyo43@gmail.com>
2020-09-19 16:34:40 +0900

    文字の入力とリストの更新

--- src/MessageFrame.java
+++ src/MessageFrame.java
@@ -1,14 +1,15 @@
-import java.awt.Frame;
-
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MessageFrame extends Frame {
+public class MessageFrame extends JFrame implements ActionListener {
     private static final long serialVersionUID = -3594659038545207881L;
 
     private JPanel messageBox;
@@ -34,10 +35,24 @@ public class MessageFrame extends Frame {
         messageInputPanel = new JPanel(new BorderLayout());
         messageInput = new JTextField();
         messageSendButton = new JButton("送信");
+        messageSendButton.addActionListener(this);
         messageInputPanel.add(messageInput, BorderLayout.CENTER);
         messageInputPanel.add(messageSendButton, BorderLayout.EAST);
         add(messageInputPanel, BorderLayout.SOUTH);
 
+        pack();
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == messageSendButton) {
+            String text = messageInput.getText();
+            if (text.equals("")) return;
+            messageBox.add(new JLabel(text));
+            messageInput.setText("");
+        }
+        messageBox.revalidate();
+        messageBox.repaint();
     }
 }
