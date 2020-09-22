3824e73112fba82c1cd3420ab470280dd7f877c3
tnyo43 <tnyo43@gmail.com>
2020-09-22 11:16:57 +0900

    吹き出しとスクロールの表示

--- src/MessageFrame.java
+++ src/MessageFrame.java
@@ -1,9 +1,13 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,24 +16,30 @@ public class MessageFrame extends JFrame implements ActionListener {
     private static final long serialVersionUID = -3594659038545207881L;
 
     private JPanel messageBox;
+    GridLayout gridLayout;
 
     private JPanel messageInputPanel;
     private JTextField messageInput;
     private JButton messageSendButton;
 
+    private JScrollPane scroll;
+
+    private static final int MINIMAM_MESSAGE_NUMBER = 7;
+    private int messageCount = 0;
+
     public MessageFrame() {
         super("メッセージフレーム");
 
         setLayout(new BorderLayout());
-        setSize(300, 300);
-
-        messageBox = new JPanel(new GridLayout(10, 1));
-        messageBox.add(new MessagePanel("ここに"));
-        messageBox.add(new MessagePanel("交換し合った"));
-        messageBox.add(new MessagePanel("メッセージを"));
-        messageBox.add(new MessagePanel("表示する"));
-        messageBox.add(new MessagePanel("予定"));
-        add(messageBox, BorderLayout.CENTER);
+        setPreferredSize(new Dimension(200,560));
+        gridLayout = new GridLayout(MINIMAM_MESSAGE_NUMBER, 1);
+        messageBox = new JPanel(gridLayout);
+        scroll = new JScrollPane(
+                    messageBox,
+                    JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                    JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
+                );
+        add(scroll, BorderLayout.CENTER);
 
         messageInputPanel = new JPanel(new BorderLayout());
         messageInput = new JTextField();
@@ -39,9 +49,18 @@ public class MessageFrame extends JFrame implements ActionListener {
         messageInputPanel.add(messageSendButton, BorderLayout.EAST);
         add(messageInputPanel, BorderLayout.SOUTH);
 
+        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
+    }
+
+    private void addMessage(String text) {
+        messageCount++;
+        if (messageCount > MINIMAM_MESSAGE_NUMBER) {
+            gridLayout.setRows(messageCount);
+        }
+        messageBox.add(new MessagePanel(text));
     }
 
     @Override
@@ -49,9 +68,18 @@ public class MessageFrame extends JFrame implements ActionListener {
         if (e.getSource() == messageSendButton) {
             String text = messageInput.getText();
             if (text.equals("")) return;
-            messageBox.add(new MessagePanel(text));
+            addMessage(text);
             messageInput.setText("");
+
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    JScrollBar scrollBar = scroll.getVerticalScrollBar();
+                    scrollBar.setValue(scrollBar.getMaximum());
+                }
+            });
         }
+
         messageBox.revalidate();
         messageBox.repaint();
     }
